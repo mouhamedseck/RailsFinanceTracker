8b42da33c828c9b7952508c4ac3b7a30dc6edcc6
Mouhamed SECK <mseckmouhamed@gmail.com>
2023-07-26 13:42:42 +0000

    Add devise user ressources

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root 'welcome/index'
+  devise_for :users
+  root 'welcome#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
