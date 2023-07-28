52d58b069633f34a17ebc13fb234104f2b69e483
Mouhamed SECK <mseckmouhamed@gmail.com>
2023-07-28 17:06:37 +0000

    Store secure API key in credenteials

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -2,8 +2,8 @@ class Stock < ApplicationRecord
 
     def self.new_lookup(ticker_symbol)
         client = IEX::Api::Client.new(
-        publishable_token: 'pk_2483a2dd284d4bb898ae3ecf327c4a9d',
-        secret_token: 'sk_642253341ce146c0bb79d001b5a4d030',
+        publishable_token: Rails.application.credentials.iex_client[:public_key],
+        secret_token: Rails.application.credentials.iex_client[:secret_key],
         endpoint: 'https://cloud.iexapis.com/v1'
         )
 
