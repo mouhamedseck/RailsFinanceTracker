b6e61a5781d7351810474743c327bcaffbc28c1d
Mouhamed SECK <mseckmouhamed@gmail.com>
2023-07-28 16:48:06 +0000

    Build class and method to lookup Stock info

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,12 @@
 class Stock < ApplicationRecord
+
+    def self.new_lookup(ticker_symbol)
+        client = IEX::Api::Client.new(
+        publishable_token: 'pk_2483a2dd284d4bb898ae3ecf327c4a9d',
+        secret_token: 'sk_642253341ce146c0bb79d001b5a4d030',
+        endpoint: 'https://cloud.iexapis.com/v1'
+        )
+
+        client.price(ticker_symbol)
+    end
 end
