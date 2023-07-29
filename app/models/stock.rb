4b9c68a7dfa58583474a859217e9dcd873d37aa3
Mouhamed SECK <mseckmouhamed@gmail.com>
2023-07-29 16:16:26 +0000

    dealing with invalid search results (Exception)

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -6,7 +6,12 @@ class Stock < ApplicationRecord
         secret_token: Rails.application.credentials.iex_client[:secret_key],
         endpoint: 'https://cloud.iexapis.com/v1'
         )
-        company = client.company(ticker_symbol)
-        new(ticker: ticker_symbol, name: company.company_name, last_price: client.price(ticker_symbol))
+        
+        begin
+            company = client.company(ticker_symbol)
+            new(ticker: ticker_symbol, name: company.company_name, last_price: client.price(ticker_symbol))
+        rescue => exception
+            return nil
+        end
     end
 end
